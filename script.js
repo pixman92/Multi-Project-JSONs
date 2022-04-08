a945e0e2da67fbdca40836f3144758d1218a8892
pixman92 <GOtrustD@verizon.net>
2022-04-07 23:53:37 -0700

    2022-04-07 - 10 minute Looping Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var theBigJSON;
+
+var theSmallJSON;
+
+function initVariables(){
+    theBigJSON = new JSON_Instance();
+    theSmallJSON = new JSON_Instance();
+}
+
+function addToBig(name){
+    theBigJSON.addToObj([[[0]])
+}
+
+
+
+
+
+// =================
+function run(){
+
+}
